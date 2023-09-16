42176de0e60acf13f0152b042aa25addaacdd9cf
asafmaoz1234 <asafmaoz27@gmail.com>
2023-09-16 17:46:26 +0300

    init project

---
+++ src/main/java/com/seavision/seavisiondatafetcher/SeaVisionDataFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.seavision.seavisiondatafetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaVisionDataFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaVisionDataFetcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seavision/seavisiondatafetcher/SeaVisionDataFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seavision.seavisiondatafetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaVisionDataFetcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
